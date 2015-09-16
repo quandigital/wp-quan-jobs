36dd86a0866fafcc450ad2d738f30004c6f0e5a6
alpipego <alpipego@gmail.com>
2015-09-16 18:52:34 +0200

    updated version number, plugin url, author

--- quan-jobs.php
+++ quan-jobs.php
@@ -4,9 +4,11 @@ namespace QuanDigital\Jobs;
 
 /**
  * Plugin Name: Quan Job Listings
- * Plugin URI: http://www.quandigital.com/
+ * Plugin URI: https://github.com/quandigital/wp-quan-jobs
+ * Author: Quan Digital GmbH
+ * Author URI: http://www.quandigital.com/
  * Description: Adds a custom post types for jobs
- * Version: 1.0.0
+ * Version: 1.1.1
  * License: MIT
  */
 
